769cc5f340975460fdf09c1aab145fdec896ccbb
Ronaldo <ronaldocruzbastos@gmail.com>
2022-12-10 16:33:44 -0300

    downgrade puppteer

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const bot = async(req) => {
     const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
         headless: true
     });
     const page = await browser.newPage();

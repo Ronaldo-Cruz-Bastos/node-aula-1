b2002aed08c3d88342a6e49030ec9188a5f4336b
Ronaldo <ronaldocruzbastos@gmail.com>
2022-12-10 16:10:11 -0300

    Aula instagram

---
+++ src/bot.js
@@ -0,0 +1,22 @@
+const puppeteer = require("puppeteer");
+const bot = async(req) => {
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+
+    await page.goto(`https://www.instagram.com/${req.params.username}/?__a=1&__d=dis`);
+
+    const imgList = await page.evaluate(() => {
+        return document.getElementsByTagName('pre')[0].textContent;
+        const nodeList = document.querySelectorAll('article img');
+        const imgArray = [...nodeList];
+        const imgList = imgArray.map(({ src }) => ({ src }));
+        return imgList;
+    });
+    await browser.close();
+    return imgList;
+
+};
+
+module.exports = bot;
--- src/index.js
+++ src/index.js
@@ -1,61 +1,26 @@
-/*const express = require("express");
+const express = require("express");
+const bot = require("./bot");
+
 const path = require("path");
 const app = express();
 const router = express.Router();
-
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/pages/home.html"));
 });
+router.get("/:username", async(req, res) => {
+    const response = await bot(req);
+    res.write('<html><head></head><body>');
 
-app.use(router);
-app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
-    console.log("rodando.....")
-});*/
+    res.write(response);
+    res.end('</body></html>');
 
-const puppeteer = require("puppeteer");
-//import puppeteer from 'puppeteer';
-
-
-
-
-const express = require("express");
-const path = require("path");
-const app = express();
-const router = express.Router();
-
-router.get("/:username", (req, res) => {
-
-    (async() => {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        await page.goto(`https://www.instagram.com/${req.params.username}/?__a=1&__d=dis`);
-
-        //Executado no puppeteer.Browser
-        const imgList = await page.evaluate(() => {
-            return document.getElementsByTagName('pre')[0].textContent;
-            const nodeList = document.querySelectorAll('article img');
-            const imgArray = [...nodeList];
-            const imgList = imgArray.map(({ src }) => ({ src }));
-            return imgList;
-        });
-
-        //await page.screenshot({ path: 'oi.png' });
-        console.log(imgList);
-
-        res.write('<html><head></head><body>');
-
-        res.write(imgList);
-        res.end('</body></html>');
-        await browser.close();
-    })();
     //res.sendFile(path.join(__dirname + "/pages/home.html"));
 });
 
 app.use(router);
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-// app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
-//     console.log("rodando.....")
+// app.listen(process.env.PORT || 3000, function() {
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 // });
+app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
+    console.log("rodando.....")
+});

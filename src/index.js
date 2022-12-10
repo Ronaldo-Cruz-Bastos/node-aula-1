68e6a8d35802200746b683d8740e9e546da8fc2d
Ronaldo <ronaldocruzbastos@gmail.com>
2022-12-10 15:52:04 -0300

    minha mensagem de commita1

--- src/index.js
+++ src/index.js
@@ -53,6 +53,9 @@ router.get("/:username", (req, res) => {
 });
 
 app.use(router);
-app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
-    console.log("rodando.....")
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+// app.listen(process.env.PORT || 3333, "0.0.0.0", () => {
+//     console.log("rodando.....")
+// });
